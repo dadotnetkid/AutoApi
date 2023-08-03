6ed5d9e1e93fc3607cd559bf9fa2e5ea77031994
Mark Christopher Cacal <markchristopher.cacal@gmail.com>
2023-08-03 12:57:51 +0800

    added

--- AutoApi.Example1Services/Services/WeatherForecastService.cs
+++ AutoApi.Example1Services/Services/WeatherForecastService.cs
@@ -7,12 +7,10 @@ namespace AutoApi.Example1Services.Services
     public class WeatherForecastService : ApplicationService, IWeatherForecastService
     {
 
-        [HttpGet]
         public string Get()
         {
             return "Hell World";
         }
-        [HttpPost]
         public string Post(WeatherForecast model)
         {
             return "Hell World";
